487f4457309f45053c55cbf448bab144ae5f8147
SorayaFerreira <sorayaferreira00123@gmail.com>
2025-09-29 16:52:14 -0400

    chore: initial commit

---
+++ bee-1001.ts
@@ -0,0 +1,10 @@
+/**
+ Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+ Data Structure and Algorithms: https://youtu.be/7kf1SACqlRw?si=3D_6RwblHG5swhRN
+ 1. Comece com um problema de array resolvido por muitas pessoas.
+    - Code Wars is an easy leetcode
+ 2. Você precisa aprender os padrões fundamentais de resolução de problemas.
+ 3. Se você demorar mais de 30 minutos (em flow) e não conseguir resolver, busque pela solução.
+ 4. Foque nos problemas de nível médio.
+ */
